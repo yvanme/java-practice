bfdddecf9d8f4445c7e197587a29778f0f9b3e65
yvanme <0744959syf>
2018-03-01 17:13:32 +0800

    对于资源文件的操作

--- dom4j/src/test/java/com/loong/XMLTest.java
+++ dom4j/src/test/java/com/loong/XMLTest.java
@@ -1,7 +1,13 @@
 package com.loong;
 
-public class XMLTest {
-    public static void main(String[] args) {
+import org.apache.log4j.Logger;
+import org.junit.Test;
 
+public class XMLTest {
+    private static Logger logger = Logger.getLogger(XMLTest.class);
+
+    @Test
+    public void testSolrUrl(){
+        //logger.info(ConstantUtils.SOLR_URL);
     }
 }

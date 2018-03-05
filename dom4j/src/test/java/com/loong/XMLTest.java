46374f1413dd9e5c2a2131afdb1d5111ce4b79f0
yvanme <0744959syf>
2018-03-05 17:52:16 +0800

    对于资源文件的操作

--- dom4j/src/test/java/com/loong/XMLTest.java
+++ dom4j/src/test/java/com/loong/XMLTest.java
@@ -1,13 +1,176 @@
 package com.loong;
 
 import org.apache.log4j.Logger;
+import org.dom4j.*;
+import org.dom4j.io.DOMReader;
+import org.dom4j.io.OutputFormat;
+import org.dom4j.io.SAXReader;
+import org.dom4j.io.XMLWriter;
 import org.junit.Test;
+import org.springframework.util.ResourceUtils;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
 
 public class XMLTest {
     private static Logger logger = Logger.getLogger(XMLTest.class);
 
+    /**
+     * 使用dom4j读取xml
+     */
     @Test
-    public void testSolrUrl(){
-        //logger.info(ConstantUtils.SOLR_URL);
+    public void testXML(){
+        try {
+            File xml = ResourceUtils.getFile("classpath:xml/2015元数据.xml");
+            logger.info(xml.getPath());
+            //创建解析器
+            SAXReader reader = new SAXReader();
+            Document document = reader.read(xml);
+            Element root = document.getRootElement();
+
+            //root属性列表
+            List<Attribute> rootList = root.attributes();
+            for(Attribute attr:rootList){
+                //root属性
+                logger.info(attr.getName() +" "+attr.getValue());
+            }
+
+            //root子元素列表
+            List<Element> childElements = root.elements();
+            for (Element child : childElements) {
+
+                //child值
+                logger.info(child.getName()+"=="+child.getText());
+
+                //child属性列表
+                List<Attribute> attributeList = child.attributes();
+                for (Attribute attr : attributeList) {
+                    //child属性
+                    logger.info(attr.getName() + ": " + attr.getValue());
+                }
+                //child子元素列表
+                List<Element> elementList = child.elements();
+                for (Element child2 : elementList) {
+                    logger.info(child2.getName() + "|| " + child2.getText());
+                }
+            }
+
+
+
+        } catch (FileNotFoundException e) {
+            logger.error("获取资源文件失败....."+e.getMessage());
+            e.printStackTrace();
+        } catch (DocumentException e) {
+            logger.error("读取xml文件失败....."+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 使用dom4j保存一个xml文件
+     */
+    @Test
+    public void saveXML(){
+        try {
+            Document doc = DocumentHelper.createDocument();
+            //增加根节点
+            Element books = doc.addElement("books");
+            //增加子元素
+            Element book1 = books.addElement("book");
+            Element title1 = book1.addElement("title");
+            Element author1 = book1.addElement("author");
+            Element book2 = books.addElement("book");
+            Element title2 = book2.addElement("title");
+            Element author2 = book2.addElement("author");
+            //为子节点添加属性
+            book1.addAttribute("id", "001");
+            //为元素添加内容
+            title1.setText("Harry Potter");
+            author1.setText("J K. Rowling");
+            book2.addAttribute("id", "002");
+            title2.setText("Learning XML");
+            author2.setText("Erik T. Ray");
+            //实例化输出格式对象
+            OutputFormat format = OutputFormat.createPrettyPrint();
+            //设置输出编码
+            format.setEncoding("UTF-8");
+            //创建需要写入的File对象
+            File file = new File("D:" + File.separator + "books1.xml");
+            //生成XMLWriter对象，构造函数中的参数为需要输出的文件流和格式
+            XMLWriter writer = new XMLWriter(new FileOutputStream(file), format);
+            //开始写入，write方法中包含上面创建的Document对象
+            writer.write(doc);
+        } catch (IOException e) {
+            logger.error("操作xml文件失败....."+e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void IteratorXML() {
+        try {
+            SAXReader saxReader = new SAXReader();
+            File xml = ResourceUtils.getFile("classpath:xml/2015元数据.xml");
+            Document document = saxReader.read(xml);
+
+            // 获取根元素
+            Element root = document.getRootElement();
+            System.out.println("Root: " + root.getName());
+
+            // 获取所有子元素
+            List<Element> childList = root.elements();
+            System.out.println("total child count: " + childList.size());
+
+            // 获取特定名称的子元素
+            List<Element> childList2 = root.elements("hello");
+            System.out.println("hello child: " + childList2.size());
+
+            // 获取名字为指定名称的第一个子元素
+            Element firstWorldElement = root.element("world");
+            // 输出其属性
+            System.out.println("first World Attr: "
+                    + firstWorldElement.attribute(0).getName() + "="
+                    + firstWorldElement.attributeValue("name"));
+
+            System.out.println("迭代输出-----------------------");
+            // 迭代输出
+            for (Iterator iter = root.elementIterator(); iter.hasNext();)
+            {
+                Element e = (Element) iter.next();
+                System.out.println(e.attributeValue("name"));
+
+            }
+
+            System.out.println("用DOMReader-----------------------");
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            // 注意要用完整类名
+            org.w3c.dom.Document document2 = db.parse(new File("D:\\student.xml "));
+
+            DOMReader domReader = new DOMReader();
+
+            // 将JAXP的Document转换为dom4j的Document
+            Document document3 = domReader.read(document2);
+
+            Element rootElement = document3.getRootElement();
+
+            System.out.println("Root: " + rootElement.getName());
+        } catch (DocumentException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
